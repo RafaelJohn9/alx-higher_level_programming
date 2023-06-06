c2f54dc67130c9c3235053b82b38a4896f7ffae9
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-06 17:02:36 +0300

    task 5: fixed bug where I was to use format()in print function

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@
 # Prints from 0 to 99
 for i in range(0, 100):
     if (i == 99):
-        print(f"{i}")
+        print("{}".format(i))
     else:
-        print(f"{i:02d},", end=(" "))
+        print("{:02d},".format(i), end=(" "))
