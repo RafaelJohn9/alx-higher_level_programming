50636d8c60d218dac80b34d3ebdc7a5eb8319b62
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-06 17:08:02 +0300

    task 8: fixed bug where I was to use format()in print function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@
 def uppercase(str):
     for char in str:
         if (ord(char) >= 97 and ord(char) <= 122):
-            print(chr(ord(char) - 32), end="")
+            print("{}".format(chr(ord(char) - 32)), end="")
         else:
-            print(f"{char}", end="")
+            print("{}".format(char), end="")
     print()
