e8a0f80dba3de40760f9d8b563e42952888e137b
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-06 16:49:59 +0300

    task 3: fixed bug where there was incorrect path to dir to python3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,7 +2,6 @@
 
 # takes the ASCII CODE converts them to char
 for i in range(97, 123):
-    # conditional statement to check for q and e and skips them
     if (i == 101 or i == 113):
         continue
     else:
