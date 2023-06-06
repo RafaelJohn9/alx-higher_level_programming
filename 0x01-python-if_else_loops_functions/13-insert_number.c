2d0e42cedcd095c28a392ec5ec6f363c484c00a3
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-06 18:00:17 +0300

    task 13:insert node with other c files for test purposes

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * insert_node-inserts node
+ * @head:the list itself
+ * @number: to be inserted
+ * Return: list with num
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *hare = (*head)->next;
+	listint_t *tortoise = *head;
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+	while (tortoise)
+	{
+		if (number < tortoise->n)
+		{
+			new_node->n = number;
+			new_node->next = tortoise;
+			return (new_node);
+		}
+		else if (!hare)
+		{
+			new_node->n = number;
+			new_node->next = NULL;
+			tortoise->next = new_node;
+			return (*head);
+		}
+		else if ((tortoise->n) < number && (hare->n) > number)
+		{
+			new_node->n = number;
+			new_node->next = hare;
+			tortoise->next = new_node;
+			return (*head);
+		}
+		else
+		{
+			tortoise = tortoise->next;
+			hare = hare->next;
+		}
+	}
+	free(new_node);
+	return (NULL);
+}
