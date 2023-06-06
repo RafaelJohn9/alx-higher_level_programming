a3436ea30c41a00c5e58196fb8b2be9aab0637da
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-06 19:19:32 +0300

    task 14: A .

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-#prints alphabet in reverse order alternating btwn lowercase and uppercase
+# prints alphabet in reverse order alternating btwn lowercase and uppercase
 for i in range(122, 96, -1):
     if (i % 2 == 0):
         print("{}".format(chr(i)), end=(""))
