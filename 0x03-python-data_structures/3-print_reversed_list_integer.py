70a5863da58c0410343590cb5aab130e828b49a0
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-12 09:33:11 +0300

    task 3: error handling where if no items are in list it returns None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
+    if length <= 0:
+        return
     idx_max = length - 1
     while idx_max >= 0:
         print("{:d}".format(my_list[idx_max]))
