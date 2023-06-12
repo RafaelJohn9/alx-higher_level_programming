d0c2ffb6e868793062c778755ae6e9980ecc7cf6
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-12 12:59:52 +0300

    task 11: deletes item in a list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+delete_at- deletes the item at a specific position in a list.
+my_list:is just a list
+Return: list
+'''
+
+
+def delete_at(my_list=[], idx=0):
+    checkingIndex = 0
+    index = 0
+    for num in my_list:
+        if checkingIndex == idx:
+            checkingIndex += 1
+            continue
+        my_list[index] = num
+        index += 1
+        checkingIndex += 1
+    my_list[-1:] = []
+    my_list = my_list[:index]
+    return my_list
