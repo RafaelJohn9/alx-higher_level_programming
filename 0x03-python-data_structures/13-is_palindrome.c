15ae29af3c2573f8df9787b61d071415bc91a086
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-12 17:35:49 +0300

    task 13: betty

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,37 +7,35 @@
  */
 int is_palindrome(listint_t **head)
 {
-        if (*head == NULL || (*head)->next == NULL)
-                return (1);
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return (1);
+	}
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
+	listint_t *temp;
 
-        listint_t *slow = *head;
-        listint_t *fast = *head;
-        listint_t *prev = NULL;
-        listint_t *temp;
-
-        while (fast != NULL && fast->next != NULL)
-        {
-                fast = fast->next->next;
-
-                // Reverse the first half of the list
-                temp = slow;
-                slow = slow->next;
-                temp->next = prev;
-                prev = temp;
-        }
-
-        // If the list has odd number of elements, skip the middle element
-        if (fast != NULL)
-                slow = slow->next;
-
-        // Compare the reversed first half with the second half
-        while (slow != NULL)
-        {
-                if (slow->n != prev->n)
-                        return (0);
-                slow = slow->next;
-                prev = prev->next;
-        }
-
-        return (1);
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		temp = slow;
+		slow = slow->next;
+		temp->next = prev;
+		prev = temp;
+	}
+	if (fast != NULL)
+	{
+		slow = slow->next;
+	}
+	while (slow != NULL)
+	{
+		if (slow->n != prev->n)
+		{
+			return (0);
+		}
+		slow = slow->next;
+		prev = prev->next;
+	}
+	return (1);
 }
