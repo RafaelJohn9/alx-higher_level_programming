e72c53628095f8dc33b8be1148de76b3d28f43fe
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-12 10:33:36 +0300

    task 8: error handling case that handles empty string.

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -8,7 +8,7 @@
 
 
 def multiple_returns(sentence):
-    if sentence[0] is None:
+    if len(sentence) == 0:
         return len(sentence), None
     else:
         return len(sentence), sentence[0]
