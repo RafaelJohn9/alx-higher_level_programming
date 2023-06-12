8dfed5d8e0fd7e62afc0a86635f8c2896a96dffd
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-12 10:44:54 +0300

    task 9:finds max integer in a list:

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''
+max_integer-searches and returns biggest int
+my_list:list to be checked
+Returns:biggest int
+'''
+
+
+def max_integer(my_list=[]):
+    maxValue = 0
+    length = len(my_list)
+    while length:
+        index = length - 1
+        if my_list[index] > maxValue:
+            maxValue = my_list[index]
+        length -= 1
+    return maxValue
