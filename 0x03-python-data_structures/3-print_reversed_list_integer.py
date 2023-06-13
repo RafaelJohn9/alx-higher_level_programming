7d1860a787bbad72bb796776b467e58bf704bdfa
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-13 08:22:04 +0300

    task 3: error handling when list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
-    if length <= 0:
+    if len(my_list) == 0:
         return
+    length = len(my_list)
     idx_max = length - 1
     while idx_max >= 0:
         print("{:d}".format(my_list[idx_max]))
