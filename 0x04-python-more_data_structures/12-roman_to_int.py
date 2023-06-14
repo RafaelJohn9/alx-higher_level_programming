d9e56117571803e8074f3bf1c71a6fc104dec53e
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-14 20:48:34 +0300

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sum = 0
+    prev = 0
+    for c in roman_string:
+        curr = roman_dict[c]
+        if curr > prev:
+            sum += curr - 2 * prev
+        else:
+            sum += curr
+        prev = curr
+    return sum
