ef55b6f565004f3f42961df2396d591f90829923
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-14 20:51:44 +0300

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    key = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
     prev = 0
     for c in roman_string:
-        curr = roman_dict[c]
+        curr = key[c]
         if curr > prev:
             sum += curr - 2 * prev
         else:
