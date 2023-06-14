834f28f9a2a878ae39215dc8e2d47ad442dc5187
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-14 18:25:21 +0300

    task9. Multiply by 2 9:

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''
+multiply_by_2:multiplies value by 2
+@a_dictionary:dictionary that returns value multiplied by 2
+'''
+
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    for key, value in zip(a_dictionary.keys(), a_dictionary.values()):
+        new_dict[key] = value * 2
+    return new_dict
