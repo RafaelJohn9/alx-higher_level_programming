941773d6ddfc77991e02757edb56407063c6315e
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-14 17:00:41 +0300

    pycodestyle task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,14 +6,16 @@ common_elements- returns a set of common elements in two sets.
 @set_2:second set
 Return: a set
 '''
+
+
 def only_diff_elements(set_1, set_2):
     new_set = set()
 
-    #insert first set
+    # insert first set
     for element_1 in set_1:
-            new_set.add(element_1)
+        new_set.add(element_1)
 
-    #inserts second set
+    # inserts second set
     for element_2 in set_2:
         if element_2 in new_set:
             new_set.remove(element_2)
