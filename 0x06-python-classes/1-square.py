d78ab9168dc31fca24fb4324b935cf7a2b6bf682
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-27 16:33:15 +0300

    task 1: documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-'''
-Square - empty class
-'''
+""" Square - contains attribute size """
 
 
 class Square:
+    """ Square - contains attribute size """
     def __init__(self, size):
         self.__size = size
