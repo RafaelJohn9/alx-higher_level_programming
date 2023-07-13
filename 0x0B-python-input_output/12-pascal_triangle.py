53d967bc0835ca7b6632a62ed739158ee50312fa
RafaelJohn9 <rafaeljohb@gmail.com>
2023-07-13 06:02:36 +0300

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""
+pascalTriangle-it prints a pascal triangle until to the n'th row
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return ([])
+
+    i = 0
+
+    pascalList = []
+    previousList = []
+    while i < n:
+        currentList = []
+        currentList.append(1)
+        if i == 0:
+            pascalList.append(currentList)
+            previousList == currentList
+            i += 1
+            continue
+        elif i == 1:
+            currentList.append(1)
+            pascalList.append(currentList)
+            previousList = currentList
+            i += 1
+            continue
+        else:
+            index = 0
+            while (index + 1 < len(previousList)):
+                currentList.append(previousList[index] + previousList[index+1])
+                index += 1
+            currentList.append(1)
+            pascalList.append(currentList)
+            previousList = currentList
+            index += 1
+        i += 1
+    return (pascalList)
