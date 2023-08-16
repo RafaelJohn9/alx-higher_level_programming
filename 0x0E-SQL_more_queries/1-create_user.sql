1b49b1fe2e347ed728b851ffce0663f4d877163c
RafaelJohn9 <rafaeljohb@gmail.com>
2023-08-16 21:15:12 +0300

    task1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- a script that creates the MYSEL server used user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'MySQL' IDENTIFIED BY 'user_0d_1';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'MySQL';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
