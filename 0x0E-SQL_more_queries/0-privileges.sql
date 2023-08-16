103e33c097b70a891b8283d5f6bf576b53711b92
RafaelJohn9 <rafaeljohb@gmail.com>
2023-08-16 20:56:02 +0300

    commented on each statement

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- shows permissions for  user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- shows permissions for  user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
