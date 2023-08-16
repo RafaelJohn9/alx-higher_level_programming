5557e9515b60b82159d330290c53be5c7af5b483
RafaelJohn9 <rafaeljohb@gmail.com>
2023-08-16 21:59:52 +0300

    task 5:creating a unique_id.id using primary key

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- a script that creates the table unique_id on your MYSQL server
-CREATE TABLE IF NOT EXISTS unique_id(id INT, `name` VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT PRIMARY KEY, `name` VARCHAR(256));
