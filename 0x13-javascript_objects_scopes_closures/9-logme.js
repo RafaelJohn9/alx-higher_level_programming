a59e31fd8ffcaa0749a1643a5ea1543757ed0be2
RafaelJohn9 <rafaeljohb@gmail.com>
2023-09-12 21:44:19 +0300

    log me: task 9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,12 +2,12 @@
 
 // a function that prints the number of arguments already printed and the new arg val.
 
-var index = 0;
+let index = 0;
 
-function logMe(str) {
-	console.log("%d: %s",index++, str);
+function logMe (str) {
+  console.log('%d: %s', index++, str);
 }
 
-module.exports={
-	logMe
+module.exports = {
+  logMe
 };
