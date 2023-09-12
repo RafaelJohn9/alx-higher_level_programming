294d9d8cf2fd3f2ced6b8133dac6ed552e78a94c
RafaelJohn9 <rafaeljohb@gmail.com>
2023-09-12 21:32:48 +0300

    task 8:corrected reversing list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,12 @@
 // a function that returns the reversed version of a list
 
 function esrever (list) {
-  return (list.reverse());
+  const length = (list.length - 1);
+  const newList = [];
+  for (let i = length; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return (newList);
 }
 
 module.exports = {
