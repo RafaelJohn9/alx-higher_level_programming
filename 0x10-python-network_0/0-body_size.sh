3519cab32082b733217afeeeb39f2b1e63c35e73
RafaelJohn9 <rafaeljohb@gmail.com>
2023-09-28 21:03:42 +0300

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,6 +2,4 @@
 # this script exctracts the content length in bits of the html document passed in curl
 
 url=$1
-content=$(curl -si "$url" | grep -i "Content-Length" | awk '{print $2}')
-
-echo "$content"
+curl -s $url | wc -c
