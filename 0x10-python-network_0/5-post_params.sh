174e80c06a99a61aa6ca8ffe41bfdb925f130566
RafaelJohn9 <rafaeljohb@gmail.com>
2023-09-29 00:26:12 +0300

    task 5:corrected the key value arg

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a bash script that takes in a URL, sends a POST request to the passed URL
-curl -sX POST $1 -d email=test@gmail.com -d subject=I%20will%20always%20be%20here%20for%20PLD
+curl -sX POST "$1" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
