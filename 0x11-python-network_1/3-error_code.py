698e550b527b3061c85be7610f18c4300d13ad4b
RafaelJohn9 <rafaeljohb@gmail.com>
2023-09-29 15:09:05 +0300

    task3: fetches a html page gives a message if it does not exist

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+a python script tthat takes in a URL sends a request to the URL and displays
+the body of the response(decoded in utf-8)
+"""
+
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import urllib.error
+    import sys
+
+    try:
+        url = sys.argv[1]
+        with urllib.request.urlopen(url) as response:
+            content = response.read().decode("utf-8")
+            print(content)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
