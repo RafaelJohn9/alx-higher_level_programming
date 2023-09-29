4607d612ce6965eae9b7d317256e5f6a5b4d00de
RafaelJohn9 <rafaeljohb@gmail.com>
2023-09-29 15:29:22 +0300

    used the request lib as per the instruction in task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,11 +5,11 @@ a python script that fetches https://alx-intranet.hbtn.io/status
 """
 
 if __name__ == "__main__":
-    import urllib.request
+    import requests
 
     url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        content = response.read().decode('utf-8')
-        print("Body response:")
-        print(f"\t- type: {type(content)}")
-        print(f"\t- content: {content}")
+    response = requests.get(url)
+    content = response.text
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
