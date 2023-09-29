5acb5c8ea79b5f6e48bb6f491b036845c6d299e1
RafaelJohn9 <rafaeljohb@gmail.com>
2023-09-29 20:33:16 +0300

    a python script that takes your github credentials and uses the github api do display the result

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+a python script that takes github credentials as arguments and uses the github
+api to display my id
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    username = sys.argv[1]
+    password ={
+            "Authorization": sys.argv[2]
+            }
+
+    url = f"https://api.github.com/users/{username}"
+
+    response = requests.get(url, headers=password)
+
+    content = response.json()
+    print(content['id'])
