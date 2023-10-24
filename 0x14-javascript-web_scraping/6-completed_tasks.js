58cedf3ddc687b91b84c9bc679c7e8103e485d03
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-24 23:02:19 +0300

    task 6: how many completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+/**
+ * a script that computes the number of tasks completed by user id
+ * the first arg is the api url
+ * only prints the users with completed task
+ * you must use the module request
+ */
+
+const request = require('request');
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+const myDict = {};
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    for (const element of data) {
+      if (element.completed === true) {
+        if (myDict[element.userId]) {
+          myDict[element.userId] += 1;
+        } else {
+          myDict[element.userId] = 1;
+        }
+      }
+    }
+    console.log(myDict);
+  }
+});
